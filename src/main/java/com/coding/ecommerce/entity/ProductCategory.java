b9ad5c37ac285def5e0462d1bf30f49deb07f3ab
amynee <tlijaniamine9@gmail.com>
2024-02-05 23:32:39 +0100

    config to prevent put, post, delete methods

--- src/main/java/com/coding/ecommerce/entity/ProductCategory.java
+++ src/main/java/com/coding/ecommerce/entity/ProductCategory.java
@@ -1,2 +1,25 @@
-package com.coding.ecommerce.entity;public class ProductCategory {
+package com.coding.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name= "product_category")
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
 }
