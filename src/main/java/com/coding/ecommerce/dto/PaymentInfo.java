b1c3aa758e9387908798d6db6c057c10e2cc18b1
amynee <tlijaniamine9@gmail.com>
2024-03-07 21:27:50 +0100

    integrate stripe

---
+++ src/main/java/com/coding/ecommerce/dto/PaymentInfo.java
@@ -0,0 +1,11 @@
+package com.coding.ecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfo {
+
+    private int amount;
+    private String currency;
+    private String receiptEmail;
+}
--- src/main/java/com/coding/ecommerce/service/CheckoutService.java
+++ src/main/java/com/coding/ecommerce/service/CheckoutService.java
@@ -1,9 +1,14 @@
 package com.coding.ecommerce.service;
 
+import com.coding.ecommerce.dto.PaymentInfo;
 import com.coding.ecommerce.dto.Purchase;
 import com.coding.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
+
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
